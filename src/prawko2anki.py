5c1125c5cbaff2653cbd9b6e824081d45be1b090
Maciej Surdziel <m.surdziel@gmail.com>
2019-09-07 19:29:52 +0200

    updated to match new format of excel file with questions. Excel file last modified on 2019-05-16

--- src/prawko2anki.py
+++ src/prawko2anki.py
@@ -8,38 +8,32 @@ import sys
 import argparse
 from pyexcel_xlsx import get_data
 
-def main(input, media, output, category):
+def main(input, media, output, category, system):
 
     if not os.path.exists(output):
         os.makedirs(os.path.join(output, "media"))
 
     #media: %APPDATA%\Anki2\{profile}\collection.media
-    #fields: pytanie, odpowiedz, blok, punkty, komentarz, zrodlo
-    template = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n"
+    #fields: pytanie, odpowiedz, punkty
+    template = "{0}\t{1}\t{2}\n"
     outfile = open(os.path.join(output, "prawo-jazdy-pytania.txt"), "w", encoding="utf-8")
-    
+    copy_command = 'copy' if system == 'windows' else 'cp'
+
     data = get_data(input)
-    for item in data["Treść pytania"]:
-        kategorie = item[18].split(',')
+    for item in data["pytania"]:
+        kategorie = item[17].split(',')
         if category in kategorie:
-            #nazwa_pytania = item[0]
-            #numer_pytania = item[1]
-            pytanie = item[2]
-            odpowiedz_a = item[3]
-            odpowiedz_b = item[4]
-            odpowiedz_c = item[5]
-            odpowiedz_poprawna = item[14]
-            media_file = item[15]
-            liczba_punktow = item[17]
-            nazwa_bloku = item[19]
-            zrodlo_pytania = item[20]
-            komentarz = item[22]
+            #numer_pytania = item[0]
+            #tresc_pytania = item[1]
+            pytanie = item[1]
+            odpowiedz_a = item[2]
+            odpowiedz_b = item[3]
+            odpowiedz_c = item[4]
+            odpowiedz_poprawna = item[13]
+            media_file = item[14]
+            liczba_punktow = item[16]
             
             if media_file:
-                
-                if media_file.endswith('.wmv'):
-                    # wrong file names in the XLSX for category A
-                    media_file = media_file[:-4] + '.mp4'
                     
                 output_media_file = os.path.join(output, "media", media_file)
                 
@@ -47,14 +41,14 @@ def main(input, media, output, category):
                     input_media_file = os.path.join(os.path.dirname(input), "media", media_file)
                     if not os.path.exists(input_media_file):
                         raise Exception("Missing media file: " + input_media_file)
-                    # Windows command
-                    os.system('copy "{0}" "{1}"'.format(input_media_file, output_media_file))
+                    
+                    os.system(copy_command + ' "{0}" "{1}"'.format(input_media_file, output_media_file))
                     if not os.path.exists(output_media_file):
                         raise Exception("Cannot copy media file: " + input_media_file)
             question = '<div class="pytanie">' + pytanie + '</div>'
             if media_file:
                 question += '<div class="media">'
-                if media_file.endswith('.mp4'):
+                if media_file.endswith(('.mp4','.wmv','.avi')):
                     question += '[sound:' + media_file + ']'
                 elif media_file.endswith('.jpg'):
                     question += '<img src="' + media_file + '" />'
@@ -85,7 +79,7 @@ def main(input, media, output, category):
             else:
                 raise Exception("Unexpected answer: " + odpowiedz_poprawna)
             answer += '</div>'
-            outfile.write(template.format(question, answer, nazwa_bloku, liczba_punktow, komentarz, zrodlo_pytania))
+            outfile.write(template.format(question, answer, liczba_punktow))
     #print(media)
     #print(output)
     outfile.close()
@@ -101,13 +95,15 @@ def parse_arguments():
         help='Output directory', required=True)
     parser.add_argument('-c', '--category', 
         help='License category', required=True)
+    parser.add_argument('-s', '--system', 
+        help='Operating system', required=False, default='windows', choices=['windows','linux'])
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
     args = parse_arguments()
     try:
-       sys.exit(main(input = args.input, media = args.media, output = args.output, category = args.category))
+        sys.exit(main(input = args.input, media = args.media, output = args.output, category = args.category, system = args.system))
     except Exception as exc:
         print(exc, file=sys.stderr, end="\n")
         sys.exit(1)
